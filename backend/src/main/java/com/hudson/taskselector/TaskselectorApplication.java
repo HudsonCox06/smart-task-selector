4efec41c57d4902ddc370f66d2a68e3db84c8e14
HudsonCox06 <hudsoncox90@gmail.com>
2026-04-02 14:27:04 -0500

    Add Spring Boot backend scaffold

---
+++ backend/src/main/java/com/hudson/taskselector/TaskselectorApplication.java
@@ -0,0 +1,13 @@
+package com.hudson.taskselector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskselectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskselectorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hudson/taskselector/TaskselectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hudson.taskselector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskselectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
